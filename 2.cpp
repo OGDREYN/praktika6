8768534fff7a6b39cca1f7468aba811a813c5569
OGDREYN <150112168+OGDREYN@users.noreply.github.com>
2023-11-14 21:57:02 +0300

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+int n = 10;
+int a = 0;
+int b = 1;
+
+cout << "Первые " << n << " чисел последовательности Фибоначчи:"<<endl;
+
+for (int i = 0; i < n; i++) 
+{
+	cout << a << " ";
+	int sum = a + b;
+	a = b;
+	b = sum;
+}
+
+return 0;
